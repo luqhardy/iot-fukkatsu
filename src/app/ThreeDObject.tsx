cfdec77a8998ef1450abb59ba0e021482a1f8f92
Luqman Hadi <102597758+luqhardy@users.noreply.github.com>
2025-07-17 21:35:13 +0900

    Update ThreeDObject.tsx

--- src/app/ThreeDObject.tsx
+++ src/app/ThreeDObject.tsx
@@ -20,9 +20,9 @@ interface SensorData {
 const fetcher = async (url: string): Promise<SensorData> => {
   const res = await fetch(url);
   if (!res.ok) {
-    const error = new Error('An error occurred while fetching the data.');
-    (error as any).info = await res.json();
-    (error as any).status = res.status;
+    const error = new Error('An error occurred while fetching the data.') as Error & { info: any, status: number };
+    error.info = await res.json();
+    error.status = res.status;
     throw error;
   }
   return res.json();
